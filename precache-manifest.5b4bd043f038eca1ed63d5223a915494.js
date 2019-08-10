9d1f9cce44e4c29cb679f4d09e6ce7c90cd59484
Israel Cruz Ibarra <israel.ibarra@ibm.com>
2019-08-09 19:58:15 -0500

    Updates

---
+++ precache-manifest.5b4bd043f038eca1ed63d5223a915494.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5939a89ebf8946e951d07d48c09849a",
+    "url": "/ReactTask/index.html"
+  },
+  {
+    "revision": "70f3c80ccb417f64b131",
+    "url": "/ReactTask/static/css/main.da9f758e.chunk.css"
+  },
+  {
+    "revision": "9956f428d0946e578ca0",
+    "url": "/ReactTask/static/js/2.01228f4b.chunk.js"
+  },
+  {
+    "revision": "70f3c80ccb417f64b131",
+    "url": "/ReactTask/static/js/main.678bc5f2.chunk.js"
+  },
+  {
+    "revision": "003717712c1240982701",
+    "url": "/ReactTask/static/js/runtime~main.9c962ff8.js"
+  }
+]);
